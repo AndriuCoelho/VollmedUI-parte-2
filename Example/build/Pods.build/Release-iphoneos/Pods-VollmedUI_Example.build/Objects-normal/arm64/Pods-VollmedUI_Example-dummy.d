517361352392dbb281f3c991d8cacca5f8718ed1
ikyrie <mah.c5r@gmail.com>
2024-12-13 17:02:48 -0300

    gerando a documentação com Jazzy

---
+++ Example/build/Pods.build/Release-iphoneos/Pods-VollmedUI_Example.build/Objects-normal/arm64/Pods-VollmedUI_Example-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/pcmacestudio/Desktop/VollmedUI-parte-2/Example/build/Pods.build/Release-iphoneos/Pods-VollmedUI_Example.build/module.modulemap \
+  /Users/pcmacestudio/Desktop/VollmedUI-parte-2/Example/Pods/Target\ Support\ Files/Pods-VollmedUI_Example/Pods-VollmedUI_Example-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
---
+++ Example/build/Pods.build/Release-iphoneos/VollmedUI.build/Objects-normal/arm64/VollmedUI_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/pcmacestudio/Desktop/VollmedUI-parte-2/Example/build/Pods.build/Release-iphoneos/VollmedUI.build/module.modulemap \
+  /Users/pcmacestudio/Desktop/VollmedUI-parte-2/Example/build/Pods.build/Release-iphoneos/VollmedUI.build/DerivedSources/VollmedUI_vers.c
